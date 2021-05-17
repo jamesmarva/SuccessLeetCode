64f1718b6c71c360346682452092bef904e8e7ea
wanghanwei <jamesmarva@users.noreply.github.com>
2021-05-18 01:59:15 +0800

    Update Pro_17_08.java

--- src/gold/Pro_17_08.java
+++ src/gold/Pro_17_08.java
@@ -3,6 +3,7 @@ package gold;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class Pro_17_08 {
@@ -72,6 +73,26 @@ public class Pro_17_08 {
         public String toString() {
             return "[h: " + height + ", w: " + weight + "]";
         }
+    }
+
+    public int bestSeqAtIndex1(int[] height, int[] weight) {
+        int len = height.length;
+        int[][] person = new int[len][2];
+        for (int i = 0; i < len; ++i)
+            person[i] = new int[]{height[i], weight[i]};
+
+        Arrays.sort(person, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
+        int[] dp = new int[len];
+        int res = 0;
+        for (int[] pair : person) {
+            int i = Arrays.binarySearch(dp, 0, res, pair[1]);
+            if (i < 0)
+                i = -(i + 1);
+            dp[i] = pair[1];
+            if (i == res)
+                ++res;
+        }
+        return res;
     }
 
 }
